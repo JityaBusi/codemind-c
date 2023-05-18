00cffc2b0e6272d3fa2a7a6a2eb835e797fa5480
JityaBusi <119723522+JityaBusi@users.noreply.github.com>
2023-05-18 20:36:50 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,k;
+    int n,i,j;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n-i;j++)
-        printf(" ");
+        for(j=1;j<=i;j++)
         {
-            for(k=1;k<2*i;k++)
-            printf("%d",i);
-            printf("
-");
+            printf("%d",j);
         }
-        
+        printf("
+");
     }
 }
