027e664ce9a5030d11194e8f535e1dd22a6e2c2a
JityaBusi <119723522+JityaBusi@users.noreply.github.com>
2023-05-29 21:56:39 +0530

    #include <stdio.h> int main() { 	int n; 	scanf("%d", &n); 	int arr[n]; 	int i; 	for(i=0; i<n; i++) 	{ 		scanf("%d", &arr[i]); 	} 	int vis[101] = {}; 	int found=0, j; 	int count=0; 	for(i=0;i<n;i++) 	{ 	    int cnt = 0; 	    for(j=0; j<n; j++) 	    { 	        if(arr[i] == arr[j]) 	        { 	            cnt++; 	        } 	    }
    	    if(arr[i] == cnt && vis[arr[i]] != 1)
    	    {
    	      count++;  
    	    }
    	    vis[arr[i]] = 1;
    	}
    	printf("%d",count);
    }

---
+++ value_same_as_count.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int arr[n];
+	int i;
+	for(i=0; i<n; i++) 
+	{
+		scanf("%d", &arr[i]); 
+	}
+	int vis[101] = {};
+	int found=0, j;
+	int count=0;
+	for(i=0;i<n;i++)
+	{
+	    int cnt = 0;
+	    for(j=0; j<n; j++)
+	    {
+	        if(arr[i] == arr[j])
+	        {
+	            cnt++;
+	        }
+	    }
+	    
+	    if(arr[i] == cnt && vis[arr[i]] != 1)
+	    {
+	      count++;  
+	    }
+	    vis[arr[i]] = 1;
+	}
+	printf("%d",count);
+}
