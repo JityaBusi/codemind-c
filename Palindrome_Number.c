9e8a19e212ec033f3034b86d955f058fc6648a65
JityaBusi <119723522+JityaBusi@users.noreply.github.com>
2023-05-29 21:12:39 +0530

    #include <stdio.h> int main() {     int m,j;     scanf("%d",&m);     for(j=1;j<=m;j++)     { 	    int n;     	scanf("%d",&n); 	    int t=n;     	int rev=0,r;     	while(n>0)     	{     		r=n%10;     		rev=rev*10+r;     		n=n/10;     	}     	if(t==rev)     	{     	    printf("True ");     	}     	else     	{     		printf("False ");     	}     } }

--- Palindrome_Number.c
+++ Palindrome_Number.c
@@ -1,28 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int i,n,a=0,r,sum=0,temp,t;
-    scanf("%d",&t);
-    for(i=1;i<=t;i++)
+    int m,j;
+    scanf("%d",&m);
+    for(j=1;j<=m;j++)
     {
-        scanf("%d",&n);
-        temp=n;
-        sum=0;
-        while(n>0)
-        {
-            r=n%10;
-            sum=(sum*10)+r;
-            n=n/10;
-        }
-        if(temp==sum)
-        {
-            printf("True");
-        }
-        else
-        {
-            printf("False");
-        }
-        printf("
+	    int n;
+    	scanf("%d",&n);
+	    int t=n;
+    	int rev=0,r;
+    	while(n>0)
+    	{
+    		r=n%10;
+    		rev=rev*10+r;
+    		n=n/10;
+    	}
+    	if(t==rev)
+    	{
+    	    printf("True
 ");
+    	}
+    	else
+    	{
+    		printf("False
+");
+    	}
     }
 }
